0256fb33575554293235075b78e4f48ec8fb5edc
Jay <me@jayxtq.xyz>
2024-08-01 10:38:46 +0100

    Update interactionCreate.ts

--- src/events/discord/interactionCreate.ts
+++ src/events/discord/interactionCreate.ts
@@ -1,28 +1,38 @@
-import { Client, Interaction } from "discord.js";
-import { commands } from "../..";
+import { Client, Interaction } from 'discord.js';
+import { commands } from '../..';
 
 export default async function (client: Client) {
-    client.on("interactionCreate", async (interaction) => {
+    client.on('interactionCreate', async (interaction) => {
         let finder: string;
-        if (!("commandName" in interaction)) {
+        if (!('commandName' in interaction)) {
             finder = interaction.customId;
         } else {
             finder = interaction.commandName;
-            interaction.isAutocomplete() ? (finder += "-autocomplete") : finder;
+            interaction.isAutocomplete() ? (finder += '-autocomplete') : finder;
         }
         const command = commands.get(finder);
         if (!command) return;
+        console.log(`Executing interaction ${finder}...`);
         try {
             await (command.run as (interaction: Interaction) => unknown)(
                 interaction,
             );
+            console.log(`Interaction ${finder} executed successfully!`);
         } catch (error) {
+            console.log(`Error while executing interaction ${finder}:`);
             console.error(error);
             if (interaction.isCommand()) {
-                await interaction.reply({
-                    content: "There was an error while executing this command!",
-                    ephemeral: true,
-                });
+                if (interaction.deferred || interaction.replied)
+                    await interaction.editReply({
+                        content:
+                            'There was an error while executing this command!',
+                    });
+                else
+                    await interaction.reply({
+                        content:
+                            'There was an error while executing this command!',
+                        ephemeral: true,
+                    });
             }
         }
     });
